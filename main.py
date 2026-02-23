25753b08ee8534e18bd4a8201782c54b5d60287b
RyhmnyRin <ryhmnyrin@gmail.com>
2026-02-23 11:54:59 +0700

    nyoba main buat control center

---
+++ main.py
@@ -0,0 +1,20 @@
+import arithmetic
+
+print("===============================================")
+print("============== KALKULATOR SUPER ===============")
+print("===============================================")
+
+print("Pilih operasi yang ingin dilakukan:")
+print("1. Aritmatika")
+print("2. Termodinamika")
+print("3. Pengukuran")
+print("4. Keluar")
+pilih = input("Masukkan pilihan (1/2/3/4): ")
+
+if pilih == "1":
+    arithmetic.hitung_aritmatika()
+elif pilih == "2":
+elif pilih == "3":
+elif pilih == "4":
+    print("Terima kasih telah menggunakan Kalkulator Super!")
+
