605c9e1595b2d17b242a5965b020a0125c0ded18
muhammadrafialrabbani25 <muhammad.rafi.tif425@polban.ac.id>
2026-02-23 11:05:42 +0700

    Menambahkan fitur aritmatika

---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+def tambah (a,b):
+    return a+b
+
+def kurang (a,b):
+    return a-b
+
+def kali (a,b):
+    return a*b
+
+def bagi (a,b):
+    if b == 0:
+        return "Error ⚠️ Tidak bisa membagi dengan 0!"
+    return a/b
+
+def modulus (a,b):
+    if b == 0:
+        return "Error ⚠️ Tidak bisa modulus dengan 0!"
+    return a%b
+
+def pangkat (a,b):
+    return a**b
+
