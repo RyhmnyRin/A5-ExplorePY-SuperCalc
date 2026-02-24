19c43bd633842bb1fc8a4fb08dfdbe82f4274c81
RyhmnyRin <ryhmnyrin@gmail.com>
2026-02-24 22:09:47 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import arithmetic
+import konversiPanjang
+import Science_Specialist
 
 print("===============================================")
 print("============== KALKULATOR SUPER ===============")
@@ -6,15 +8,17 @@ print("===============================================")
 
 print("Pilih operasi yang ingin dilakukan:")
 print("1. Aritmatika")
-print("2. Termodinamika")
-print("3. Pengukuran")
+print("2. Scienctific")
+print("3. Konversi Satuan Panjang")
 print("4. Keluar")
 pilih = input("Masukkan pilihan (1/2/3/4): ")
 
 if pilih == "1":
     arithmetic.hitung_aritmatika()
 elif pilih == "2":
+    Science_Specialist.menu_science()
 elif pilih == "3":
+    konversiPanjang.konversi_Panjang()
 elif pilih == "4":
     print("Terima kasih telah menggunakan Kalkulator Super!")
 
