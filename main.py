80fd8454d472f9b0c1e23ff813b92d97be195185
RyhmnyRin <ryhmnyrin@gmail.com>
2026-02-24 22:13:42 +0700

    added informatics tools

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arithmetic
 import konversiPanjang
 import Science_Specialist
+import informtatics_tools
 
 print("===============================================")
 print("============== KALKULATOR SUPER ===============")
@@ -10,8 +11,9 @@ print("Pilih operasi yang ingin dilakukan:")
 print("1. Aritmatika")
 print("2. Scienctific")
 print("3. Konversi Satuan Panjang")
-print("4. Keluar")
-pilih = input("Masukkan pilihan (1/2/3/4): ")
+print("4. Informatika")
+print("0. Keluar")
+pilih = input("Masukkan pilihan (1/2/3/4/0): ")
 
 if pilih == "1":
     arithmetic.hitung_aritmatika()
@@ -20,5 +22,7 @@ elif pilih == "2":
 elif pilih == "3":
     konversiPanjang.convert_panjang()
 elif pilih == "4":
+    informtatics_tools.menu_informatika()
+elif pilih == "0":
     print("Terima kasih telah menggunakan Kalkulator Super!")
 
